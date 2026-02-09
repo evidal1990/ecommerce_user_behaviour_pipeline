c024bc5de32ceb06969489c3ed78fbad74741eb7
Edith Vidal <ecsr.vidal@gmail.com>
2026-02-09 15:27:19 -0300

    Adicionando função e teste para adicionar pasta ao cloud storage

--- storage/gcs.py
+++ storage/gcs.py
@@ -4,10 +4,22 @@ from google.auth import default
 credentials, project = default()
 
 
-def get_blobs(bucket_name):
+def get_blobs(bucket):
     blobs_list = []
     client = storage.Client()
-    blobs = client.bucket(bucket_name).list_blobs()
+    blobs = client.bucket(bucket).list_blobs()
     for blob in blobs:
         blobs_list.append(blob.name)
     return blobs_list
+
+
+def add_blob(bucket, blob_name):
+    client = storage.Client()
+    bucket = client.bucket(bucket)
+    bucket.blob(f"{blob_name}/").upload_from_string("")
+
+
+def del_blob(bucket, blob_name):
+    client = storage.Client()
+    bucket = client.bucket(bucket)
+    bucket.blob(f"{blob_name}/").delete()
--- tests/gcp/bucket_test.py
+++ tests/gcp/bucket_test.py
@@ -1,16 +1,26 @@
 from google.auth import default
 from src.utils import get_env_variables, get_yaml_data
 from storage import gcs
+from datetime import datetime
 
 credentials, project = default()
 environment = get_env_variables.load()
-yaml_data = get_yaml_data.get(environment)
-bucket_name = yaml_data["cloud_storage"]["bucket"]
+cloud_storage = get_yaml_data.get(environment)["cloud_storage"]
+bucket = cloud_storage["bucket"]
+paths = cloud_storage["paths"]
 
 
-def test_main_folders():
-    blobs = gcs.get_blobs(yaml_data["cloud_storage"]["bucket"])
+def test_get_blobs():
+    blobs = gcs.get_blobs(bucket)
     assert "raw/" in blobs
     assert "bronze/" in blobs
     assert "silver/" in blobs
     assert "gold/" in blobs
+
+
+def test_add_blob():
+    blob = f'{paths["raw"]}{datetime.now()}'
+    gcs.add_blob(bucket=bucket, blob_name=blob)
+    blobs = gcs.get_blobs(bucket)
+    assert f'{blob}/' in blobs
+
