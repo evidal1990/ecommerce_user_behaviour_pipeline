710eb0bbe36ea0f4529bf151c6f4a42a026ce6f3
Edith Vidal <ecsr.vidal@gmail.com>
2026-02-11 22:30:35 -0300

    Implementando a orquestração e ajustando o script de execução da pipeline

--- scripts/run_pipeline.py
+++ scripts/run_pipeline.py
@@ -1,37 +1,30 @@
+# scripts/run_pipeline.py
 import logging
 from pathlib import Path
-from src.ingestion.csv_ingestion import CsvIngestion
+from src.orchestration.pipeline import Pipeline
 from src.utils import file_io
 
 BASE_DIR = Path(__file__).resolve().parents[1]
 
 
-def _load_global_settings() -> dict:
-    return file_io.read_yaml(BASE_DIR / "config" / "settings.yaml")
-
-
-def _setup_logging() -> None:
+def main() -> None:
     log_path = BASE_DIR / "logs" / "pipeline.log"
     log_format = "%(asctime)s | %(levelname)s | %(message)s"
+
     logging.basicConfig(
-        level=logging.INFO, filemode="w", format=log_format, filename=log_path
+        level=logging.INFO, format=log_format, filename=log_path, filemode="w"
     )
 
-
-def execute_pipeline() -> None:
-    _setup_logging()
-    logging.info("Iniciando o pipeline...")
-
     try:
-        global_settings = _load_global_settings()
+        logging.info("Iniciando o pipeline...")
+        settings = file_io.read_yaml(BASE_DIR / "config" / "settings.yaml")
+        Pipeline(settings).run()
+        logging.info("Pipeline finalizada com sucesso.")
 
-        CsvIngestion(global_settings).execute()
-
-    except Exception as exception:
-        logging.exception(f"Falha na execução do pipeline {exception}")
+    except Exception:
+        logging.exception("Falha na execução do pipeline")
         raise
 
-    logging.info("Pipeline finalizada com sucesso.")
 
-
-execute_pipeline()
+if __name__ == "__main__":
+    main()
---
+++ src/orchestration/pipeline.py
@@ -0,0 +1,13 @@
+# src/orchestration/pipeline.py
+import logging
+from src.ingestion.csv_ingestion import CsvIngestion
+
+
+class Pipeline:
+    def __init__(self, settings: dict) -> None:
+        self.settings = settings
+
+    def run(self) -> None:
+        logging.info("Ingestão de CSV iniciada...")
+        CsvIngestion(self.settings).execute()
+        logging.info("Ingestão de CSV finaliza...")
