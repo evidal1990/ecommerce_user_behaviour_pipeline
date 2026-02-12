4b4bf31498cccf162f606f240f633c99f45e7ab4
Edith Vidal <ecsr.vidal@gmail.com>
2026-02-11 22:57:53 -0300

    Melhorando file_io e csv_ingestion

--- src/utils/file_io.py
+++ src/utils/file_io.py
@@ -1,28 +1,10 @@
 import yaml
-import polars as pl
 from typing import Any
 
 
 def read_yaml(path) -> Any:
-    try:
-        with open(path, "r", encoding="utf-8") as f:
-            data = yaml.safe_load(f)
+    with open(path, "r", encoding="utf-8") as f:
+        data = yaml.safe_load(f)
 
-        return data
+    return data
 
-    except Exception as excepetion:
-        raise FileNotFoundError(f"Arquivo não encontrado em {path}") from excepetion
-
-
-def read_csv(path: str) -> pl.DataFrame:
-    try:
-        return pl.read_csv(path)
-    except Exception as excepetion:
-        raise FileNotFoundError(f"Arquivo não encontrado em {path}") from excepetion
-
-
-def write_csv(df: pl.DataFrame, path: str) -> None:
-    try:
-        df.write_csv(path)
-    except Exception as exception:
-        raise RuntimeError(f"Falha ao escrever CSV em {path}") from exception
--- tests/file_io/file_io_test.py
+++ tests/file_io/file_io_test.py
@@ -1,18 +1,5 @@
 import pytest
-import polars as pl
-from random import random
 from src.utils import file_io
-
-
-def test_read_valid_csv() -> None:
-    df = file_io.read_csv("tests/data/test.csv")
-    assert df is not None
-
-
-def test_read_not_found_csv() -> None:
-    path = "tests/data/invalid.csv"
-    with pytest.raises(FileNotFoundError, match=f"Arquivo não encontrado em {path}"):
-        file_io.read_csv(path)
 
 
 def test_read_valid_yaml() -> None:
@@ -22,14 +9,5 @@ def test_read_valid_yaml() -> None:
 
 def test_read_not_found_yaml() -> None:
     path = "tests/data/invalid.yaml"
-    with pytest.raises(FileNotFoundError, match=f"Arquivo não encontrado em {path}"):
+    with pytest.raises(FileNotFoundError):
         file_io.read_yaml(path)
-
-
-def test_write_csv() -> None:
-    df_path = "tests/data/new.csv"
-    df = pl.DataFrame({"col1": random(), "col2": random(), "col3": random()})
-    file_io.write_csv(df=df, path=df_path)
-    df_read = pl.read_csv(df_path)
-    assert df_read is not None
-    assert df_read.equals(df)
