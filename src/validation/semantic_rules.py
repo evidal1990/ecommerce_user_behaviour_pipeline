abbbc6549e45ea620a9b39cbae5339737820bfc3
Edith Vidal <ecsr.vidal@gmail.com>
2026-02-18 22:01:22 -0300

    Verificando se há valores duplicados na coluna user_id na classe SemanticRules

--- src/validation/semantic_rules.py
+++ src/validation/semantic_rules.py
@@ -1,4 +1,5 @@
 import polars as pl
+import logging
 
 
 class SemanticRules:
@@ -6,4 +7,18 @@ class SemanticRules:
         self.df = df
 
     def execute(self) -> None:
-        pass
+        self._check_duplicated_user_id()
+
+    def _check_duplicated_user_id(self) -> None:
+        logging.info("Verificando duplicidade de user_id...")
+        duplicated_ids = (
+            self.df["user_id"]
+            .filter(self.df["user_id"].is_duplicated())
+            .unique()
+            .to_list()
+        )
+        if duplicated_ids:
+            logging.error(f"user_id duplicados encontrados: {duplicated_ids}")
+        else:
+            logging.info("Coluna user_id sem valores duplicados")
+        logging.info("Verificação de duplicidade de user_id concluida.")
