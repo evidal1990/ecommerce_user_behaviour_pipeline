7a959e6cd5b9119091d235250afd4ea030ef7dd0
Edith Vidal <ecsr.vidal@gmail.com>
2026-02-25 13:19:40 -0300

    1

--- src/validation/duplicated_user_id.py
+++ src/validation/duplicated_user_id.py
@@ -4,6 +4,7 @@ from consts.validation_status import ValidationStatus
 from validation.semantic_rule import SemanticRule
 from src.utils import dataframe, statistics
 
+
 class DuplicatedUserId(SemanticRule):
     def __init__(self, column: str = "user_id", sample_size: int = 10):
         self.column = column
@@ -21,19 +22,21 @@ class DuplicatedUserId(SemanticRule):
             .to_list()
         )
         users_total = len(users)
-        invalid_percentage = statistics.get_percentage(users_total, total_records)
-        sample = (
-            dataframe.get_df_sample(df, self.column, self.sample_size, users_total)
-            if users_total > 0
-            else []
-        )
+        if users_total > 0:
+            status = ValidationStatus.PASS
+            sample = []
+            percentage = 0.0
+        else:
+            status = ValidationStatus.FAIL
+            sample = dataframe.get_df_sample(
+                df, self.column, self.sample_size, users_total
+            )
+            percentage = statistics.get_percentage(users_total, total_records)
         return {
-            "status": (
-                ValidationStatus.FAIL if users_total > 0 else ValidationStatus.PASS
-            ),
+            "status": status,
             "error_code": ErrorCode.DUPLICATED_USER_ID,
             "total_records": total_records,
             "invalid_records": users_total,
-            "invalid_percentage": invalid_percentage,
+            "invalid_percentage": percentage,
             "sample": sample,
         }
